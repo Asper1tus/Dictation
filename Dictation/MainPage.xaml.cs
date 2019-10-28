027cf9fab3940fd7cc41746e79ef7e857005b19b
Qr1stal <dmitriy.timakhov@gmail.com>
2019-10-28 17:37:24 +0200

    Implement MVVM patten, add menu page

--- Dictation/MainPage.xaml.cs
+++ Dictation/MainPage.xaml.cs
@@ -1,133 +1,27 @@
 ﻿namespace Dictation
 {
-    using System;
-    using System.Text;
-    using Windows.Media.SpeechRecognition;
-    using Windows.UI.Core;
+    using Dictation.ViewModels;
+    using Dictation.Views;
     using Windows.UI.Xaml;
     using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Media.Animation;
     using Windows.UI.Xaml.Navigation;
 
     public sealed partial class MainPage : Page
     {
-        private SpeechRecognizer speechRecognizer;
-        private CoreDispatcher dispatcher;
-        private StringBuilder dictatedTextBuilder;
-        private bool isListening = false;
-        private bool isVisible = false;
+        Frame rootFrame;
+        public MainPageViewModel Logic { get; } = new MainPageViewModel();
 
         public MainPage()
         {
             InitializeComponent();
-            isListening = true;
-            dictatedTextBuilder = new StringBuilder();
+            rootFrame = Window.Current.Content as Frame;
         }
 
-        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
         {
-            dispatcher = CoreWindow.GetForCurrentThread().Dispatcher;
-            speechRecognizer = new SpeechRecognizer();
-            SpeechRecognitionCompilationResult result =
-                await speechRecognizer.CompileConstraintsAsync();
-            speechRecognizer.ContinuousRecognitionSession.ResultGenerated +=
-        ContinuousRecognitionSession_ResultGenerated;
-            speechRecognizer.ContinuousRecognitionSession.Completed +=
-      ContinuousRecognitionSession_Completed;
-            speechRecognizer.HypothesisGenerated +=
-                SpeechRecognizer_HypothesisGenerated;
+            rootFrame.Navigate(typeof(Menu), null, new SlideNavigationTransitionInfo() { Effect = SlideNavigationTransitionEffect.FromLeft });
         }
 
-        private async void AppBarToggleButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (isListening)
-            {
-                if (speechRecognizer.State == SpeechRecognizerState.Idle)
-                {
-                    await speechRecognizer.ContinuousRecognitionSession.StartAsync();
-                }
-            }
-            else
-            {
-                if (speechRecognizer.State != SpeechRecognizerState.Idle)
-                {
-                    await speechRecognizer.ContinuousRecognitionSession.StopAsync();
-                }
-            }
-
-            isListening = !isListening;
-        }
-
-        private async void ContinuousRecognitionSession_ResultGenerated(
-      SpeechContinuousRecognitionSession sender,
-      SpeechContinuousRecognitionResultGeneratedEventArgs args)
-        {
-            if (args.Result.Confidence == SpeechRecognitionConfidence.Medium ||
-              args.Result.Confidence == SpeechRecognitionConfidence.High)
-            {
-                dictatedTextBuilder.Append(args.Result.Text + " ");
-
-                await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-                {
-                    editor.Document.SetText(Windows.UI.Text.TextSetOptions.None, dictatedTextBuilder.ToString());
-                });
-            }
-            else
-            {
-                await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-                {
-                    editor.Document.SetText(Windows.UI.Text.TextSetOptions.None, dictatedTextBuilder.ToString());
-                });
-            }
-        }
-
-        private async void ContinuousRecognitionSession_Completed(
-      SpeechContinuousRecognitionSession sender,
-      SpeechContinuousRecognitionCompletedEventArgs args)
-        {
-            if (args.Status != SpeechRecognitionResultStatus.Success)
-            {
-                if (args.Status == SpeechRecognitionResultStatus.TimeoutExceeded)
-                {
-                    await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-                    {
-                        editor.Document.SetText(Windows.UI.Text.TextSetOptions.None, dictatedTextBuilder.ToString());
-                    });
-                }
-                else
-                {
-                    await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-                    {
-                        editor.Document.SetText(Windows.UI.Text.TextSetOptions.None, dictatedTextBuilder.ToString());
-                    });
-                }
-            }
-        }
-
-        private async void SpeechRecognizer_HypothesisGenerated(
-  SpeechRecognizer sender,
-  SpeechRecognitionHypothesisGeneratedEventArgs args)
-        {
-            string hypothesis = args.Hypothesis.Text;
-            string textboxContent = dictatedTextBuilder.ToString() + " " + hypothesis + " ...";
-
-            await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-            {
-                editor.Document.SetText(Windows.UI.Text.TextSetOptions.None, textboxContent);
-            });
-        }
-
-        private void AppBarButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (isVisible)
-            {
-                FindAndReplaceBox.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                FindAndReplaceBox.Visibility = Visibility.Collapsed;
-            }
-
-            isVisible = !isVisible;
-        }
     }
 }
---
+++ Dictation/Views/Menu.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Dictation.Views
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class Menu : Page
+    {
+        Frame rootFrame;
+        public Menu()
+        {
+            this.InitializeComponent();
+            rootFrame = Window.Current.Content as Frame;
+        }
+
+        private void NavigationViewItem_Tapped(object sender, RoutedEventArgs e)
+        {
+
+            rootFrame.Navigate(typeof(MainPage), null, new SlideNavigationTransitionInfo() { Effect = SlideNavigationTransitionEffect.FromRight });
+        }
+    }
+}
